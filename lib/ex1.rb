d9fe550f9fa0faec677e889f224386d1c45a285d
Nathan Keller <keller.nathan@gmail.com>
2021-02-09 12:21:24 -0700

    few other examples

---
+++ lib/ex1.rb
@@ -0,0 +1,45 @@
+def solution(a, b)
+  a = a.split(//)
+  b = b.split(//)
+  accum = 0
+
+  if a.length == b.length
+    a.uniq do |a_letter|
+      if b.include?(a_letter) == false
+        accum +=1
+      end
+    end
+
+    b.uniq do |b_letter|
+      if a.include?(b_letter) == false
+        accum +=1
+      end
+    end
+    return accum
+  else
+    if a.length != a.uniq.length
+      accum += (a.length.to_i - a.uniq.length.to_i)
+    end
+
+    if b.length != b.uniq.length
+      accum += (b.length.to_i - b.uniq.length.to_i)
+    end
+    
+    a.uniq do |a_letter|
+      if b.include?(a_letter) == false
+        accum +=1
+      end
+    end
+
+    b.uniq do |b_letter|
+      if a.include?(b_letter) == false
+        accum +=1
+      end
+    end
+  end
+  accum
+end
+
+a = 'harder'
+b = 'rather'
+puts solution(a, b)
---
+++ test/ex1_test.rb
@@ -0,0 +1,6 @@
+require 'minitest/autorun'
+require './lib/ex1'
+
+class Ex1Test < Minitest::Test
+
+end
