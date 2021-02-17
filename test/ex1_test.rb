f947dffa121492bb4924a326802cfa2ea4e8bc34
Nathan Keller <keller.nathan@gmail.com>
2021-02-17 11:56:53 -0700

    Add a o test

--- test/ex1_test.rb
+++ test/ex1_test.rb
@@ -10,12 +10,26 @@ class Ex1Test < Minitest::Test
     a = 'harder'
     b = 'rather'
     x = solution(a, b)
+    # need to add a d and t
 
     assert_equal x, 2
   end
 
-  def method_name
+  def test_go_and_no
+    a = 'go'
+    b = 'no'
+    x = solution(a,b)
+    # need to add a g and n
 
+    assert_equal x, 2
+  end
+
+  def test_a_zero_scenero
+    a = 'cat'
+    b = 'tac'
+    x = solution(a,b)
+
+    assert_equal x, 0
   end
 
 end
